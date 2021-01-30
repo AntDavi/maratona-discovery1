ac91fd14b4b8dfb156edafe9dcfb3ffe45ff553f
Anthony Davi <ant.davi07@gmail.com>
2021-01-30 17:53:28 -0300

    aula 3

--- script.js
+++ script.js
@@ -133,8 +133,44 @@ const Utils = {
 }
 
 const Form = {
+    description: document.querySelector('input#description'),
+    amount: document.querySelector('input#amount'),
+    date: document.querySelector('input#date'),
+
+    getValues() {
+        return {
+            description: Form.description.value,
+            amount: Form.amount.value,
+            date: Form.date.value,
+        }
+    },
+
+    validateField() {
+        const { description, amount, date} = Form.getValues()
+
+        if(
+            description.trim() === "" || 
+            amount.trim() === "" ||
+            date.trim() === ""
+        ) {
+            throw new Error ("Por favor preencha todos os campos.")
+        }
+    },
+
     submit(event) {
         event.preventDefault()
+
+        try {
+            //Verificar se todas as informaçõe foram preenchidas
+            Form.validateField()
+            // formatar os dados para salvar
+            //salvar
+            //apagar os dados do formulario
+            // fechar Modal
+            //Atualizar aplicação
+        } catch (error) {
+            alert(error.message);
+        }
     }
 }
 
