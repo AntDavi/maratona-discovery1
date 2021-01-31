08594e46968d29e5b90aacbade6888e7cb2dcddd
Anthony Davi <ant.davi07@gmail.com>
2021-01-31 13:29:57 -0300

    aula 3

--- script.js
+++ script.js
@@ -74,12 +74,14 @@ const DOM = {
 
     addTransaction (transaction, index) {
         const tr = document.createElement('tr')
-        tr.innerHTML = DOM.innerHTMLTransaction(transaction)
+        tr.innerHTML = DOM.innerHTMLTransaction(transaction, index)
+
+        tr.dataset.index = index
 
         DOM.transactionsContainer.appendChild(tr)
     },
 
-    innerHTMLTransaction (transaction) {
+    innerHTMLTransaction (transaction, index) {
         const CSSclass = transaction.amount > 0 ? "income" : "expense"
 
         const amount = Utils.formatCurrency(transaction.amount)
@@ -90,7 +92,7 @@ const DOM = {
                 <td class="${CSSclass}">${amount}</td>
                 <td class="date">${transaction.date}</td>
                 <td>
-                    <img src="/assets/minus.svg" alt="Remover transação">
+                    <img onclick="Transaction.remove(${index})" src="/assets/minus.svg" alt="Remover transação">
                 </td>
             </tr>
         `
@@ -210,9 +212,7 @@ const Form = {
 const App = {
     init () {
 
-        Transaction.all.forEach(transaction => {
-            DOM.addTransaction(transaction)
-        })
+        Transaction.all.forEach(DOM.addTransaction)
         
         DOM.updateBalance()
     },
